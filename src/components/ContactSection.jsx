56da508c955c2cb5f1f0c4ff3f5e78889a5fb4b1
V-DevX <vasanthjany@gmail.com>
2025-05-04 11:03:03 +0530

    updated UI

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -110,7 +110,7 @@ function ContactSection() {
 
           {/* Social Icons */}
           <motion.div
-            className="my-2 flex flex-wrap items-center gap-2"
+            className="my-2 flex flex-wrap items-center gap-[0.01rem]"
             variants={staggerContainer}
             initial="hidden"
             whileInView="visible"
